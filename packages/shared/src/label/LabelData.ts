32a2a517ca47874de2640d911a66e59242c96557
yuleisheng <shengyulei@gmail.com>
2025-02-19 09:07:18 -0800

    cleanup SFT label code

--- packages/shared/src/label/LabelData.ts
+++
@@ -1,5 +0,0 @@
-import { Database } from '~shared/schema.gen';
-
-export type LabelData = Database['public']['Tables']['label_data']['Row'];
-
-export type LabelDataWithAttachmentUrls = { data: LabelData; attachmentUrls: string[] };
