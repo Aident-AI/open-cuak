100e2e489bdafabd0bbb277544f2260b9f731246
yuleisheng <shengyulei@gmail.com>
2025-03-03 10:45:52 +0800

    feat: pt2 get rid of role selection, unnecessary complexity for users

--- apps/web/src/components/chat-box/AiMessageTeachModeInput.tsx
+++ apps/web/src/components/chat-box/AiMessageTeachModeInput.tsx
@@ -1,7 +1,7 @@
 import { PaperAirplaneIcon } from '@heroicons/react/24/solid';
 import { Message } from 'ai';
 import cx from 'classnames';
-import { FormEvent, useEffect, useState } from 'react';
+import { FormEvent } from 'react';
 import { v4 as UUID } from 'uuid';
 import { GrowableTextArea } from '~src/components/GrowableTextArea';
 
@@ -11,16 +11,7 @@ interface Props {
   append: (message: Message) => void | Promise<void>;
 }
 
-type Role = 'user' | 'assistant' | 'system';
-
 export function AiMessageTeachModeInput({ formRef, messages, append }: Props) {
-  const [role, setRole] = useState<Role>('user');
-
-  useEffect(() => {
-    if (messages.length < 1) setRole('user');
-    else setRole('assistant');
-  }, [messages]);
-
   const inputHistory = messages.map((m) => m.content);
 
   const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
@@ -29,18 +20,19 @@ export function AiMessageTeachModeInput({ formRef, messages, append }: Props) {
     const formData = new FormData(e.currentTarget);
     const input = (formData.get('message') as string).trim();
     if (!input || input.length < 1) return;
-    await append({ role, content: input, id: UUID() });
+    await append({ role: 'user', content: input, id: UUID() });
 
     formRef.current?.reset();
   };
 
   const renderUserInstruction = () => {
+    if (messages.length > 0) return null;
     return (
       <>
         <text className="flex w-full items-center justify-center text-white/70 underline">Instruction</text>
         <div className="flex w-full flex-col items-start px-4 py-2 text-sm text-white/70">
           <ol className="list-decimal pl-4">
-            <li>Describe the workflow or task on a high level</li>
+            <li>Describe the workflow on a high level</li>
             <li>Click the record button to start to teach Aiden</li>
             <li>Do the workflow in the browser</li>
             <li>During the workflow, add comments that you think are helpful for Aiden to understand</li>
@@ -48,24 +40,6 @@ export function AiMessageTeachModeInput({ formRef, messages, append }: Props) {
         </div>
       </>
     );
-  };
-  const renderRolePicker = () => {
-    if (messages.length < 1) return null;
-
-    const renderPicker = (r: Role, i: number) => (
-      <button
-        key={'role-picker-' + i}
-        className={cx(
-          'font-sm ml-1 rounded-xl border-2 border-white px-2 py-0.5 first:ml-0',
-          role === r ? 'bg-white text-black' : 'bg-transparent text-white',
-        )}
-        onClick={() => setRole(r)}
-      >
-        {r}
-      </button>
-    );
-
-    return <div className="mb-2">{(['user', 'assistant', 'system'] as Role[]).map(renderPicker)}</div>;
   };
 
   return (
@@ -76,7 +50,6 @@ export function AiMessageTeachModeInput({ formRef, messages, append }: Props) {
       )}
     >
       {renderUserInstruction()}
-      {renderRolePicker()}
       <form
         ref={formRef}
         className="flex h-fit w-full flex-row items-center justify-center rounded-md bg-sky-600/95 p-1.5 shadow-centered shadow-fuchsia-600/50 backdrop-blur-sm"
@@ -89,7 +62,7 @@ export function AiMessageTeachModeInput({ formRef, messages, append }: Props) {
           formRef={formRef}
           history={inputHistory}
           name="message"
-          placeholder="Define your task goal..."
+          placeholder="Describe the workflow on a high level..."
           placeholderTextColor="placeholder:text-white/30"
           textColor="text-white"
         />
